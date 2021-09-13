56de1eb40f2d2b04dcc7db37e771d3a4ba1593e4
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2021-09-13 09:45:44 +0800

    fix: add changes to train.py and add initialize checkpoints and weights

--- train.py
+++ train.py
@@ -16,9 +16,24 @@ if __name__ == "__main__":
     dict_args = vars(hparams)
     
     datamod = ClassifyDataModule(**dict_args)
-    model = MobileNetV2(**dict_args)
+    mobilenetv2 = MobileNetV2(**dict_args)
     
-    model_checkpoint = ModelCheckpoint(monitor="val_step_loss")
+    # model_checkpoint = ModelCheckpoint(monitor="val_step_loss")
+    
+    model_checkpoint = pl.callbacks.ModelCheckpoint(
+        dirpath='checkpoint/',
+        save_top_k=1,
+        filename="mobilenet_v2-{epoch:02d}-{val_step_loss:.2f}-{val_step_acc:.2f}",
+        verbose=True,
+        monitor='val_step_loss',
+        mode='min',
+    )
+    
     trainer = pl.Trainer.from_argparse_args(hparams, callbacks=model_checkpoint)
-    trainer.fit(model, datamod)
+    trainer.fit(mobilenetv2, datamod)
+    trainer.save_checkpoint("checkpoints/latest.ckpt")
+    # mobilenetv2.model.state_dict()
+    
+    
+    
     
